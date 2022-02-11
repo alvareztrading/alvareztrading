bfeaea2c656d4859741bd1172cef45d46de1c430
alvareztrading <98801680+alvareztrading@users.noreply.github.com>
2022-02-11 12:24:27 +0500

    Add files via upload

--- alvaraiz_custom/models/models.py
+++ alvaraiz_custom/models/models.py
@@ -9,11 +9,10 @@ from odoo.exceptions import ValidationError , UserError
 class SaleOrderInh(models.Model):
     _inherit = "sale.order.line"
 
-    weight = fields.Float(string="Weight")
+    weight = fields.Float(string="Total Weight", compute='_compute_weight')
 
-    @api.onchange('product_id')
-    def _onchange_fill_weight(self):
+    def _compute_weight(self):
         for record in self:
-            record.weight = record.product_id.weight
+            record.weight = record.product_id.weight * record.product_uom_qty
 
 
